bb88837e4521b5ee99b99efa1e4cf33ded76e60e
Jakub Miłek <Jakub.Milek@microsoft.wsei.edu.pl>
2023-02-05 23:51:34 +0100

    Site layout

--- ITSM/wwwroot/js/site.js
+++ ITSM/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
